a7491fe87f06d4e755e9a89143c63670efe46fb2
Pascu Maria-Antonia-Emanuela <100427829+antoniamae@users.noreply.github.com>
2023-05-19 15:47:28 +0300

    'update'

--- script_creare_tabele.sql
+++ script_creare_tabele.sql
@@ -15,7 +15,15 @@ updated_at DATE,
 CONSTRAINT fk_city_country_id FOREIGN KEY (country_id) REFERENCES countries(id)
 )
 /
-
+create table airports (
+    id int not null primary key,
+    name varchar2(50) not null,
+    city_id int not null,
+    created_at date,
+    updated_at date,
+    constraint fk_airport_city_id foreign key (city_id) references cities(id)
+)
+/
 
 create table crew (
     id int not null primary key,
@@ -36,3 +44,15 @@ create table employees (
     constraint fk_employee_crew_id foreign key (crew_id) references crew(id)
 )
 /
+create table flights (
+    id int not null primary key,
+    destination varchar2(100),
+    departure_city_id integer not null,
+    arrival_city_id integer not null,
+    departure_date date,
+    arrival_date date,
+    airplane_id integer not null,
+    employees_no integer not null,
+    constraint fk_departure_city_id foreign key (departure_city_id) references cities(id),
+    constraint fk_arrival_city_id foreign key (arrival_city_id) references cities(id)
+)
