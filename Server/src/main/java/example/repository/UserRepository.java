420a8bebf17118c450e06f8806ddc5c40b219e33
Pascu Maria-Antonia-Emanuela <100427829+antoniamae@users.noreply.github.com>
2023-05-23 19:26:27 +0300

    'mesaj'

---
+++ Server/src/main/java/example/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package example.repository;
+
+import example.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
