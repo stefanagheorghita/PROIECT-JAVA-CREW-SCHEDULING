3b66cca9e888e3fcab50c0df9ff642aa5caba667
Pascu Maria-Antonia-Emanuela <100427829+antoniamae@users.noreply.github.com>
2023-05-23 19:57:40 +0300

    another commit

--- Server/src/main/java/example/controller/RegistrationController.java
+++ Server/src/main/java/example/controller/RegistrationController.java
@@ -16,15 +16,50 @@ public class RegistrationController {
         return "register";
     }
 
+
     @PostMapping("/register")
     @ResponseBody
-    public String registerUser(String username, String password) {
-        System.out.println("Username: " + username);
+    public String register(@RequestBody RegisterForm registerForm) {
+        String employeeId = registerForm.getEmployeeId();
+        String password = registerForm.getPassword();
+
+        // Process the registration data
+        // ...
+
+        System.out.println("Employee ID: " + employeeId);
         System.out.println("Password: " + password);
 
         // Redirect to a success page or perform any other desired action
-        return "redirect:/success";
+        return "Registration successful";
     }
+
+    // Inner class for the registration form
+    public static class RegisterForm {
+        private String employeeId;
+        private String password;
+
+        // Default constructor
+        public RegisterForm() {
+        }
+
+        // Getters and setters
+        public String getEmployeeId() {
+            return employeeId;
+        }
+
+        public void setEmployeeId(String employeeId) {
+            this.employeeId = employeeId;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
 
     @GetMapping("/success")
     public String showSuccessPage() {
--- Server/src/main/java/example/service/UserService.java
+++ Server/src/main/java/example/service/UserService.java
@@ -1,16 +1,19 @@
 package example.service;
 
 import example.model.entity.User;
+import example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
     public void registerUser(User user) {
-        String hashedPassword = AppSecurityConfig.getPasswordEncoder().encode(user.getPassword());
-        user.setPassword(hashedPassword);
-        userRepository.save(user);
+       // String hashedPassword = AppSecurityConfig.getPasswordEncoder().encode(user.getPassword());
+       // user.setPassword(hashedPassword);
+        //userRepository.save(user);
     }
 }
