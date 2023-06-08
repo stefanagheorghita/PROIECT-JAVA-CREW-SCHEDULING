43d93c1224388db0c448c57e84936e37cd1e8f9e
Pascu Maria-Antonia-Emanuela <100427829+antoniamae@users.noreply.github.com>
2023-06-08 20:01:26 +0300

    tests

---
+++ Server/src/test/java/example/solution/PilotsDistributionTest.java
@@ -0,0 +1,221 @@
+package example.solution;
+
+import example.model.entity.City;
+import example.model.entity.Country;
+import example.model.entity.Employee;
+import example.model.entity.Flight;
+import example.repository.CityRepository;
+import example.repository.FlightRepository;
+import example.repository.PilotRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class PilotsDistributionTest {
+
+    @Mock
+    private FlightRepository flightRepository;
+
+    @Mock
+    private PilotRepository pilotRepository;
+
+    @Mock
+    private CityRepository cityRepository;
+
+    @InjectMocks
+    private PilotsDistribution pilotsDistribution;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        MockitoAnnotations.initMocks(this);
+        //pilotsDistribution = new PilotsDistribution();
+        //pilotsDistribution.setFlightRepository(flightRepository);
+        //pilotsDistribution.setPilotRepository(pilotRepository);
+        //pilotsDistribution.setCityRepository(cityRepository);
+    }
+
+    @Test
+    public void testLoadCitiesFromDatabase() {
+        List<City> cityEntities = new ArrayList<>();
+        cityEntities.add(new City(1, "City1", new Country(1,"Country1")));
+        cityEntities.add(new City(2, "City2", new Country(2, "Country2")));
+
+        when(cityRepository.findAll()).thenReturn(cityEntities);
+
+        List<City> cities = pilotsDistribution.loadCitiesFromDatabase();
+
+        assertEquals(2, cities.size());
+        assertEquals("City1", cities.get(0).getName());
+        //assertEquals("Country1", cities.get(0).getCountry().getName());
+        assertEquals("City2", cities.get(1).getName());
+        //assertEquals("Country2", cities.get(1).getCountry().getName());
+
+        verify(cityRepository, times(1)).findAll();
+    }
+
+//    @Test
+//    public void testBuildGraph() {
+//        List<Flight> flights = new ArrayList<>();
+//        flights.add(new Flight(1, new City(1, "City1", new Country(1, "Country1")), new City(2, "City2", new Country(2, "Country2")), DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0), 100));
+//        flights.add(new Flight(2, new City(2, "City2", new Country(2, "Country2")), new City(3, "City3", new Country(3, "Country3")), DayOfWeek.TUESDAY, LocalTime.of(13, 0), LocalTime.of(15, 0), 150));
+//
+//        List<City> cities = new ArrayList<>();
+//        cities.add(new City(1, "City1", new Country(1, "Country1")));
+//        cities.add(new City(2, "City2", new Country(2, "Country2")));
+//        cities.add(new City(3, "City3", new Country(3, "Country3")));
+//
+//        Graph graph = pilotsDistribution.buildGraph(flights, cities);
+//
+//        assertNotNull(graph);
+//
+//        assertEquals(3, graph.getNodes().size());
+//        assertTrue(graph.getNodes().contains(1));
+//        assertTrue(graph.getNodes().contains(2));
+//        assertTrue(graph.getNodes().contains(3));
+//
+//        //assertEquals(2, graph.getEdges(1).size());
+//        assertEquals(1, graph.getEdges(2).size());
+//        assertEquals(0, graph.getEdges(3).size());
+//
+//        verify(flightRepository, times(1)).findAll();
+//    }
+
+    @Test
+    public void testFindAvailablePilot() {
+        List<Employee> pilots = new ArrayList<>();
+        pilots.add(new Employee(1, "Pavel", "Adrian", null, null, null, null, null));
+        pilots.add(new Employee(2, "Andries", "Victor", null, null, null, null, null));
+
+        pilots.get(0).setAssignments(3);
+        pilots.get(1).setAssignments(5);
+
+        Employee availablePilot = pilotsDistribution.findAvailablePilot(pilots);
+
+        assertNotNull(availablePilot);
+        assertEquals(1, availablePilot.getId());
+    }
+
+    @Test
+    public void testFindFlightById() {
+        Flight flight = new Flight(1, new City(1, "City1", new Country(1, "Country1")), new City(2, "City2", new Country(2, "Country2")), DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0), 100);
+
+        when(flightRepository.findFlightById(1)).thenReturn(flight);
+
+        Flight foundFlight = pilotsDistribution.findFlightById(1);
+
+        assertNotNull(foundFlight);
+        assertEquals(1, foundFlight.getId());
+        assertEquals("City1", foundFlight.getDepartureCity().getName());
+        assertEquals("City2", foundFlight.getArrivalCity().getName());
+        assertEquals(DayOfWeek.MONDAY, foundFlight.getDepartureDay());
+        assertEquals(LocalTime.of(10, 0), foundFlight.getDepartureTime());
+        assertEquals(LocalTime.of(12, 0), foundFlight.getArrivalTime());
+        assertEquals(100, foundFlight.getAproxPassengers());
+
+        verify(flightRepository, times(1)).findFlightById(1);
+    }
+
+//    @Test
+//    public void testDistributePilots() {
+//        List<Employee> pilots = new ArrayList<>();
+//        Employee pilot1 = new Employee(1, "Pavel", "Adrian", null, null, null, null, null);
+//        Employee pilot2 = new Employee(2, "Andries", "Victor", null, null, null, null, null);
+//        pilots.add(pilot1);
+//        pilots.add(pilot2);
+//
+//        List<Flight> flights = new ArrayList<>();
+//        Flight flight1 = new Flight(1, new City(1, "City1", new Country(1, "Country1")), new City(2, "City2", new Country(2, "Country2")), DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0), 100);
+//        Flight flight2 = new Flight(2, new City(2, "City2", new Country(2, "Country2")), new City(3, "City3", new Country(3, "Country3")), DayOfWeek.TUESDAY, LocalTime.of(13, 0), LocalTime.of(15, 0), 150);
+//        flights.add(flight1);
+//        flights.add(flight2);
+//
+//        when(flightRepository.findAll()).thenReturn(flights);
+//        //when(pilotRepository.findAll()).thenReturn(pilots);
+//
+//        //pilotsDistribution.distributePilots(pilots);
+//
+//        //assertEquals(1, flight1.getPilotId());
+//        //assertEquals(2, flight2.getPilotId());
+//
+//        assertEquals(1, pilot1.getAssignments());
+//        assertEquals(2, pilot2.getAssignments());
+//
+//        verify(flightRepository, times(1)).findAll();
+//    }
+
+    @Test
+    public void testVerifyFree_PilotAvailable() {
+        List<Employee> pilots = new ArrayList<>();
+        Employee pilot1 = new Employee(1, "John", "Doe", null, null, null, null, null);
+        Employee pilot2 = new Employee(2, "Alice", "Smith", null, null, null, null, null);
+        pilots.add(pilot1);
+        pilots.add(pilot2);
+
+        Flight flight = new Flight(1, new City(1, "City1", new Country(1, "Country1")), new City(2, "City2", new Country(2,"Country2")), DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0), 100);
+
+        pilot1.getFlights().add(flight);
+
+        //List<Employee> availablePilots = pilotsDistribution.verifyFree(pilots, flight);
+
+//        assertEquals(1, availablePilots.size());
+//        assertEquals(pilot2, availablePilots.get(0));
+    }
+
+    @Test
+    public void testVerifyFree_PilotNotAvailable() {
+        List<Employee> pilots = new ArrayList<>();
+        Employee pilot1 = new Employee(1, "Ifimie", "Cerasela", null, null, null, null, null);
+        Employee pilot2 = new Employee(2, "Munteanu", "Alex", null, null, null, null, null);
+        pilots.add(pilot1);
+        pilots.add(pilot2);
+
+        Flight flight = new Flight(1, new City(1, "City1", new Country(1,"Country1")), new City(2, "City2", new Country(2, "Country2")), DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0), 100);
+
+        pilot1.getFlights().add(flight);
+        pilot2.getFlights().add(flight);
+
+//        List<Employee> availablePilots = pilotsDistribution.verifyFree(pilots, flight);
+//
+//        assertTrue(availablePilots.isEmpty());
+    }
+
+    @Test
+    public void testFindAvailablePilot_PilotAvailable() {
+        List<Employee> pilots = new ArrayList<>();
+        Employee pilot1 = new Employee(1, "Nastasa", "Vlad", null, null, null, null, null);
+        Employee pilot2 = new Employee(2, "Stoian", "Viorela", null, null, null, null, null);
+        pilots.add(pilot1);
+        pilots.add(pilot2);
+
+        pilot1.setAssignments(2);
+        pilot2.setAssignments(1);
+
+        Employee availablePilot = pilotsDistribution.findAvailablePilot(pilots);
+
+        assertEquals(pilot2, availablePilot);
+    }
+
+    @Test
+    public void testFindFlightById_FlightFound() {
+        Flight flight = new Flight(1, new City(1, "City1", new Country(1, "Country1")), new City(2, "City2", new Country(2, "Country2")), DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0), 100);
+        when(flightRepository.findFlightById(1)).thenReturn(flight);
+
+        Flight foundFlight = pilotsDistribution.findFlightById(1);
+
+        assertEquals(flight, foundFlight);
+    }
+
+}
+
---
+++ Server/src/test/java/example/solution/PlaneAllocatorGraphTest.java
@@ -0,0 +1,153 @@
+package example.solution;
+
+import example.model.entity.Airplane;
+import example.model.entity.Flight;
+import example.model.entity.PlaneLocation;
+import example.repository.AirplaneRepository;
+import example.repository.FlightRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class PlaneAllocatorGraphTest {
+
+    @Mock
+    private AirplaneRepository airplaneRepository;
+
+    @Mock
+    private FlightRepository flightRepository;
+
+    private PlaneAllocatorGraph planeAllocatorGraph;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        planeAllocatorGraph = new PlaneAllocatorGraph();
+        //planeAllocatorGraph.setAirplaneRepository(airplaneRepository);
+        //planeAllocatorGraph.setFlightRepository(flightRepository);
+    }
+
+    @Test
+    public void testAllocatePlanes() {
+        List<Flight> flights = new ArrayList<>();
+        flights.add(new Flight(1, null, null, null, null, null, null));
+
+        List<Airplane> airplanes = new ArrayList<>();
+        airplanes.add(new Airplane(1, 100));
+
+        when(flightRepository.findAll()).thenReturn(flights);
+        when(airplaneRepository.findAll()).thenReturn(airplanes);
+
+        //HashMap<Flight, Airplane> allocations = planeAllocatorGraph.allocatePlanes();
+
+    }
+
+
+    @Test
+    public void testAllocateWeights() {
+        List<Flight> flights = new ArrayList<>();
+        flights.add(new Flight(1, null, null, null, null, null, null));
+
+        List<Airplane> airplanes = new ArrayList<>();
+        airplanes.add(new Airplane(1, 100));
+
+        when(flightRepository.findAll()).thenReturn(flights);
+        when(airplaneRepository.findAll()).thenReturn(airplanes);
+
+        //planeAllocatorGraph.allocateWeights();
+
+        //Map<Flight, HashMap<Airplane, Double>> flightAirplaneWeightMap = planeAllocatorGraph.getFlightAirplaneWeightMap();
+        //assertEquals(1, flightAirplaneWeightMap.size());
+
+        //HashMap<Airplane, Double> airplaneWeightMap = flightAirplaneWeightMap.get(flights.get(0));
+        //assertEquals(1, airplaneWeightMap.size());
+
+        //Double weight = airplaneWeightMap.get(airplanes.get(0));
+        //assertEquals(2.0, weight);
+    }
+
+    @Test
+    public void testMax() {
+
+    }
+
+    @Test
+    public void testAssignments() {
+        List<Flight> flights = new ArrayList<>();
+        flights.add(new Flight(1, null, null, null, null, null, null));
+
+        List<Airplane> airplanes = new ArrayList<>();
+        airplanes.add(new Airplane(1, 100));
+
+        when(flightRepository.findAll()).thenReturn(flights);
+        when(airplaneRepository.findAll()).thenReturn(airplanes);
+
+        //planeAllocatorGraph.assignments();
+
+
+    }
+
+    @Test
+    public void testCalculateWeight() {
+        Flight flight = new Flight(1, null, null, DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(12, 0), 80);
+        Airplane airplane = new Airplane(1, 100);
+
+        PlaneLocation planeLocation = new PlaneLocation(1, 2, DayOfWeek.MONDAY, LocalTime.of(9, 0));
+        //planeAllocatorGraph.getPlaneLocations().add(planeLocation);
+
+        //double weight = planeAllocatorGraph.calculateWeight(flight, airplane);
+
+        //assertEquals(1.5, weight);
+    }
+
+    @Test
+    public void testFindPlaneLocation() {
+        PlaneLocation planeLocation1 = new PlaneLocation(1, 2, DayOfWeek.MONDAY, LocalTime.of(9, 0));
+        PlaneLocation planeLocation2 = new PlaneLocation(2, 3, DayOfWeek.TUESDAY, LocalTime.of(13, 0));
+        //planeAllocatorGraph.getPlaneLocations().add(planeLocation1);
+        //planeAllocatorGraph.getPlaneLocations().add(planeLocation2);
+
+        //PlaneLocation foundLocation = planeAllocatorGraph.findPlaneLocation(1);
+
+        //assertEquals(planeLocation1, foundLocation);
+    }
+
+    @Test
+    public void testCalculateTimeDifference() {
+        LocalTime time1 = LocalTime.of(9, 0);
+        LocalTime time2 = LocalTime.of(10, 0);
+
+        //Duration difference = planeAllocatorGraph.calculateTimeDifference(time1, time2);
+
+        //assertEquals(1, difference.toHours());
+    }
+
+    // Setter methods for dependencies
+
+    private void setAirplaneRepository(AirplaneRepository airplaneRepository) {
+        this.airplaneRepository = airplaneRepository;
+    }
+
+    private void setFlightRepository(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
+    // Getter methods for private fields
+
+    //private Map<Flight, HashMap<Airplane, Double>> getFlightAirplaneWeightMap() {
+        //return flightAirplaneWeightMap;
+    //}
+
+    //private Set<PlaneLocation> getPlaneLocations() {
+        //return planeLocations;
+    //}
+}
